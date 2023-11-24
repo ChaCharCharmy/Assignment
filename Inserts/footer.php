deff6ff45501134c4cc6e7a4d61323da01fa2525
ChaCharCharmy <charm.sciberras@gmail.com>
2023-11-25 00:06:24 +0100

    test

---
+++ Inserts/footer.php
@@ -0,0 +1,25 @@
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+        <ul>
+                <li>
+                    <a href="">Legal Documents</a>
+                </li>
+                <li>
+                    <a href="">Important Stuff</a>
+                </li>                
+                <li>
+                    <a href="">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <img src="https://i.pinimg.com/736x/cd/f3/2d/cdf32d4376b340a1a97df9018bc7a127.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
